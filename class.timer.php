f6c43e46cc4523123090375a01e3dcd533c37267
Dima Tsvetkov <web@dima.fi>
2016-12-08 12:06:23 +0200

    Update class.timer.php

--- class.timer.php
+++ class.timer.php
@@ -3,6 +3,7 @@
 class timer {
 	private $starttime;
 	private $stoptime;
+	public $data;
 
 	function __construct() {
 		$mtime = microtime(); 
@@ -11,15 +12,18 @@ class timer {
 		$this->starttime = $mtime; 
 	}
 		
-	function now($type = null) {
+	function now($title = null, $type = null) {
 		$mtime = microtime(); 
 		$mtime = explode(" ",$mtime); 
 		$mtime = $mtime[1] + $mtime[0]; 
 		$this->stoptime = $mtime; 
 
 		$totaltime = ($this->stoptime - $this->starttime); 
-		if($type == "min") return round(($totaltime/60), 2);
-		else return round($totaltime, 3);
+		if($type == "min") $time = round(($totaltime/60), 2)." min";
+		else $time = round($totaltime, 3)." sec";
+		
+		if($title) $this->data[$title] = $time;
+		return $time;
 	}
 
 	function last($type = null) {
